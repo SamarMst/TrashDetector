00695f7d7d8f1900e20215ca8ec86a608cfa2220
Samar_Mestiri <95316871+SamarMst@users.noreply.github.com>
2025-05-17 19:17:20 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import './index.css';
 import Footer from './pages/footer';
 import Hero from './pages/hero';
-import Hero1 from './pages/hero1';
 import Navbar from './pages/navbar';
 
 function App() {
@@ -10,7 +9,7 @@ function App() {
   return (
     <div className="overflow-x-hidden">
   <Navbar />
-  <Hero1 />
+  <Hero />
   <Footer />
 </div>
 
